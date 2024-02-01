4e75eb8e39f4e4e02f23a2460e8f87b907826122
leceanYS <135338781+leceanYS@users.noreply.github.com>
2024-02-01 20:44:30 +0300

    feat: add-controllers

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,16 +1,7 @@
 package ru.practicum.shareit.request.dto;
 
-import lombok.Data;
-
-import java.time.LocalDateTime;
-
 /**
  * TODO Sprint add-item-requests.
  */
-@Data
 public class ItemRequestDto {
-    long id;
-    String description;
-    long requestor;
-    LocalDateTime created;
 }
