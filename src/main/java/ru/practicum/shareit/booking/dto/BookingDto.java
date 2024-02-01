4e82217312330d657fc36e3c36b970cadb492b22
leceanYS <135338781+leceanYS@users.noreply.github.com>
2024-02-01 20:44:30 +0300

    feat: add-controllers

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,19 +1,7 @@
 package ru.practicum.shareit.booking.dto;
 
-import lombok.Data;
-import ru.practicum.shareit.booking.BookingStatus;
-
-import java.time.LocalDateTime;
-
 /**
  * TODO Sprint add-bookings.
  */
-@Data
 public class BookingDto {
-    long id;
-    LocalDateTime start;
-    LocalDateTime end;
-    Long item;
-    Long booker;
-    BookingStatus status;
 }
--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,27 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.Data;
-
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-@Data
+/**
+ * TODO Sprint add-controllers.
+ */
 public class ItemDto {
-    private long id;
-    @NotEmpty(message = "Имя не может быть null")
-    @Size(min = 1, max = 50)
-    private String name;
-    @NotEmpty(message = "Описание не может быть null")
-    @Size(min = 1, max = 200)
-    private String description;
-    @NotNull(message = "Поле available не может быть null")
-    private Boolean available;
-
-    public ItemDto(long id, String name, String description, Boolean available) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.available = available;
-    }
 }
