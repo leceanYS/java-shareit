eb400390176ea0b20435625d0c5702422a88764c
leceanYS <135338781+leceanYS@users.noreply.github.com>
2024-02-21 00:20:03 +0300

    feat: add-controllerss 3

--- src/main/java/ru/practicum/shareit/item/controllers/ItemErrorHandler.java
+++ src/main/java/ru/practicum/shareit/item/controllers/ItemErrorHandler.java
@@ -21,7 +21,7 @@ import java.util.Map;
 public class ItemErrorHandler {
 
     @ExceptionHandler({MethodArgumentNotValidException.class, ValidationException.class})
-    public ResponseEntity handle(final Exception ex) {
+    public ResponseEntity handleIncorrectUserException(final Exception ex) {
         log.error("Error occurred: {}", ex.getMessage(), ex);
 
         Map<String, String> error = new HashMap<>();
