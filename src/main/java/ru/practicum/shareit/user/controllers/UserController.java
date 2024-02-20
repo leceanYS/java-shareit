bbedb811ca831174ea21b25318fc9f85a268a776
leceanYS <135338781+leceanYS@users.noreply.github.com>
2024-02-20 13:24:52 +0300

    feat: add-controllerss 3

--- src/main/java/ru/practicum/shareit/user/controllers/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controllers/UserController.java
@@ -14,9 +14,6 @@ import ru.practicum.shareit.user.service.UserService;
 
 import java.util.List;
 
-/**
- * TODO Sprint add-controllers.
- */
 @RestController
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
