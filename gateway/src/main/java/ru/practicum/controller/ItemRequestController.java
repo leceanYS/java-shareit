a5786424ca7456512a253ae4b13b33bf6e726d53
leceanYS <135338781+leceanYS@users.noreply.github.com>
2024-03-20 22:54:02 +0300

    Add docker (#14)
    * add-bookings 1
    
    * add-bookings 1
    
    * add-bookings 1
    
    * add-bookings 1
    
    * add-bookings 1
    
    * add-bookings 1
    
    * add-bookings 4
    
    * add-bookings 5
    
    * add-bookings 5
    
    * add-item-requests
    главное не забыть по правкам со спринта
    
    * add-item-requests
    
    * add-item-requests
    
    * add-item-requests 1 1 1
    
    * add-item-requests 1 1 1
    
    * add-item-requests 1 1 1
    
    * add-item-requests 2 2 2
    
    * add-item-requests 2 2 2
    
    * add-item-requests 2 2 2
    
    * add-item-requests 3 3 3
    
    * add-item-requests 3 2 1
    
    * add-item-requests 3 2 1
    
    * add-item-requests 3 2 1
    
    * add-item-requests 3 2 2
    
    * add-item-requests 3 2 3
    
    * add-item-requests 3 2 3
    
    * add-item-requests 3 3 3
    
    * add-item-requests 3 3 3
    
    * add-item-requests 3 3 3
    
    * add-item-requests 3 3 3
    
    * add-item-requests 3 3 3
    
    * add-item-requests 3 3 3
    
    * add-item-requests 3 3 3
    
    * add-item-requests 3 3 3
    
    * add-item-requests 3 3 3
    
    * add-item-requests 3 3 3
    
    * add-item-requests 4 4 4
    
    * feat:ТЗ 16 1/10
    
    * feat:ТЗ 16 вроде все
    
    * feat:ТЗ 16 правки...
    
    * feat:ТЗ 16 правки...

---
+++ gateway/src/main/java/ru/practicum/controller/ItemRequestController.java
@@ -0,0 +1,48 @@
+package ru.practicum.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.client.ItemRequestClient;
+import ru.practicum.dto.ItemRequestDtoReceived;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+
+@Controller
+@RequestMapping(path = "/requests")
+@Validated
+@RequiredArgsConstructor
+public class ItemRequestController {
+
+    private final ItemRequestClient itemRequestClient;
+    private static final String HEADER_USER_ID = "X-Sharer-User-Id";
+
+
+    @PostMapping
+    public ResponseEntity<Object> addRequest(@RequestHeader(HEADER_USER_ID) @Min(0) long userId,
+                                             @RequestBody @Valid ItemRequestDtoReceived requestDto) {
+        return itemRequestClient.addRequest(userId, requestDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> findListRequestUser(@RequestHeader(HEADER_USER_ID) @Min(0) long userId) {
+        return itemRequestClient.findListRequestUser(userId);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Object> findListRequest(@RequestHeader(HEADER_USER_ID) @Min(0) long userId,
+                                                         @Min(0) @RequestParam(defaultValue = "0")  int from,
+                                                         @Min(1) @RequestParam(defaultValue = "10")  int size) {
+        return itemRequestClient.findListRequest(userId, from, size);
+    }
+
+    @GetMapping("/{requestId}")
+    public ResponseEntity<Object> findItemRequest(@RequestHeader(HEADER_USER_ID) @Min(0) long userId,
+                                                   @PathVariable("requestId") @Min(0) long requestId) {
+
+        return itemRequestClient.findItemRequest(userId, requestId);
+    }
+}
